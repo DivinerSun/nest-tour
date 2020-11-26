75b2927767c3b7a4f42d86ad98c4542017afcdb4
Diviner <sqq_job@sina.com>
2020-11-27 00:52:09 +0800

    feat: 官网文档学习-middlewares

--- src/modules/all.module.ts
+++ src/modules/all.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from '../middlewares/logger.middleware';
 import { CatsModule } from './cats/cats.module';
 import { HelloModule } from './hello/hello.module';
 
 @Module({
   imports: [HelloModule, CatsModule],
 })
-export class AllModule {}
+export class AllModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+  }
+}
